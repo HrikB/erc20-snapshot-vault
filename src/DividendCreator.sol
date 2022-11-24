ede83d4e6c369ef8ce27a6391ec2060c2a20b7e5
HrikB <hrik.bhowal@gmail.com>
2022-11-24 07:48:17 -0500

    contract for public permissionless dividend creation

---
+++ src/DividendCreator.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "./Vault.sol";
+
+contract DividendCreator {
+    address public vault;
+
+    uint256 rateLimit;
+    uint256 lastTime;
+
+    constructor(Vault _vault, uint256 _rateLimit) {
+        vault = _vault;
+        rateLimit = _rateLimit;
+    }
+
+    function tryCreateDividend() external {
+        require(
+            block.timestamp - lastTime > rateLimit,
+            "DividendCreator: Rate limit exceeded"
+        );
+        lastTime = block.timestamp;
+        vault.createDividend();
+    }
+}
